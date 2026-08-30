1e8694a6f5af40c76622382386d6fc4827a906de
vasjabojanic <vasja@kappastar.com>
2026-08-30 17:44:44 +0200

    V1

---
+++ Customer.cs
@@ -0,0 +1,17 @@
+public class Customer
+{
+    public string id { get; set; }
+    public string? name { get; set; }
+    public string? address { get; set; }
+    public string? headquarters { get; set; }
+    public string? country { get; set; }
+    public string? telephone_1 { get; set; }
+    public string? email_1 { get; set; }
+    public string? cre_date { get; set; }
+    public string? cha_date { get; set; }
+
+    public Customer(string ParId)
+    {
+        id = ParId;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -35,6 +37,57 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast")
 .WithOpenApi();
+
+app.MapPost("/CustomerEcho", (Customer cust) =>
+{
+    return Results.Json(new
+    {
+        Message = $"Customer {cust.name} is on address {cust.address}."
+    });
+});
+
+app.MapPost("/Customer", async (HttpRequest request) =>
+{
+    try
+    {
+        using var reader = new StreamReader(request.Body);
+        var body = await reader.ReadToEndAsync();
+
+        // Optional: parse JSON manually
+        var jsonDoc = JsonDocument.Parse(body);
+        if(jsonDoc.RootElement.TryGetProperty("id", out JsonElement idElement))
+        {
+            var id = idElement.GetString();
+            var cust = new Customer(id);
+            if(jsonDoc.RootElement.TryGetProperty("name", out JsonElement nameElement))
+                cust.name = nameElement.GetString();
+            if(jsonDoc.RootElement.TryGetProperty("address", out JsonElement addressElement))
+                cust.address = addressElement.GetString();
+            if(jsonDoc.RootElement.TryGetProperty("headquarters", out JsonElement headquartersElement))
+                cust.headquarters = headquartersElement.GetString();
+            if(jsonDoc.RootElement.TryGetProperty("country", out JsonElement countryElement))
+                cust.country = countryElement.GetString();
+            if(jsonDoc.RootElement.TryGetProperty("telephone_1", out JsonElement telephone_1Element))
+                cust.telephone_1 = telephone_1Element.GetString();
+            if(jsonDoc.RootElement.TryGetProperty("email_1", out JsonElement email_1Element))
+                cust.email_1 = email_1Element.GetString();
+            if(jsonDoc.RootElement.TryGetProperty("cre_date", out JsonElement cre_dateElement))
+                cust.cre_date = cre_dateElement.GetString();
+            if(jsonDoc.RootElement.TryGetProperty("cha_date", out JsonElement cha_dateElement))
+                cust.cha_date = cha_dateElement.GetString();
+
+            return Results.Ok(new {UpdatedId = cust.id});
+        }
+        else
+        {
+            return Results.BadRequest(new {Msg = "Request has to have ID"});            
+        }
+    }
+    catch (Exception ex)
+    {
+        return Results.BadRequest(new { Error = ex.Message });
+    }
+});
 
 app.Run();
 
